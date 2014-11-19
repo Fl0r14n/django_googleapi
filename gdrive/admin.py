e6d035b0907210c9276ea7ad0b0a92217d69dff3
Florian Chis <florian.chis@gmail.com>
2014-11-19 09:30:35 +0200

    show fields in admin

--- gdrive/admin.py
+++ gdrive/admin.py
@@ -3,7 +3,11 @@ from models import CredentialsModel
 
 
 class CredentialsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'credential')
+    search_fields = ('id__username', 'id__email')
+
+    def has_add_permission(self, request):
+        return False
 
 admin.site.register(CredentialsModel, CredentialsAdmin)
 
